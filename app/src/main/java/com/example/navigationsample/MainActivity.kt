964f67dbb3ed2936fc15227fe3c6ad7a472c006b
pleasezoomout <sebastiansalamanca@Sebastians-MacBook-Air.local>
2024-07-29 22:56:26 -0400

    set up app

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -7,9 +7,11 @@ import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.navigationsample.ui.theme.NavigationSampleTheme
 
@@ -19,11 +21,14 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             NavigationSampleTheme {
+                // A surface container using the 'background' color from the theme
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                   Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
+                       Greeting(
+                           name = "Android",
+                           modifier = Modifier.padding(innerPadding)
+                       )
+                   }
                 }
             }
         }
@@ -36,12 +41,4 @@ fun Greeting(name: String, modifier: Modifier = Modifier) {
         text = "Hello $name!",
         modifier = modifier
     )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    NavigationSampleTheme {
-        Greeting("Android")
-    }
 }
