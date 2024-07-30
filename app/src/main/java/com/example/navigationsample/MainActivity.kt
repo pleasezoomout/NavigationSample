b73d956f6bf2801b238007af6a33bbb1ef1fc81f
pleasezoomout <sebastiansalamanca@Sebastians-MacBook-Air.local>
2024-07-29 23:07:17 -0400

    move from one screen to another

--- app/src/main/java/com/example/navigationsample/MainActivity.kt
+++ app/src/main/java/com/example/navigationsample/MainActivity.kt
@@ -10,10 +10,17 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.example.navigationsample.ui.theme.NavigationSampleTheme
+import eu.tutorials.navigationsample.FirstScreen
+import eu.tutorials.navigationsample.SecondScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -24,7 +31,7 @@ class MainActivity : ComponentActivity() {
                 // A surface container using the 'background' color from the theme
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                    Surface(modifier = Modifier.fillMaxSize(), color = Color.White) {
-                       Greeting(
+                       MyApp(
                            name = "Android",
                            modifier = Modifier.padding(innerPadding)
                        )
@@ -35,10 +42,21 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyApp(name: String, modifier: Modifier = Modifier){
+    val navController = rememberNavController()
+    NavHost(navController = navController, startDestination = "firstscreen"){
+        composable("firstscreen"){
+            FirstScreen {name->
+                navController.navigate("secondscreen/$name")
+            }
+        }
+        composable(route = "secondscreen/{name}"){
+            val name = it.arguments?.getString("name") ?: "no name"
+            SecondScreen(name) {
+                navController.navigate("firstscreen")
+            }
+        }
+    }
 }
